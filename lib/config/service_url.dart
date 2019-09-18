f4a0303863d84a6aab1e8c61fb9f5e2793d71708
CoderSamz <developersamz@outlook.com>
2019-09-19 01:49:07 +0800

    完成21-23节的内容

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -2,6 +2,8 @@ const serviceUrl = 'https://wxmini.baixingliangfan.cn/baixing/';
 const servicePath = {
   // 商家首页信息
   'homePageContext':serviceUrl + 'wxmini/homePageContent',
-  // 商城首页
+  // 商城首页信息
   'homePageBelowConten': serviceUrl+'wxmini/homePageBelowConten',
+  // 商品类别信息
+  'getCategory': serviceUrl+'wxmini/getCategory',
 };
