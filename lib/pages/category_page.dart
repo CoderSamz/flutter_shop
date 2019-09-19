44d0b8fba6844239ddee9238506f93effa92a2bf
CoderSamz <developersamz@outlook.com>
2019-09-19 23:23:26 +0800

    完成25节内容

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -12,12 +12,22 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    _getCategory();
+//    _getCategory();
     return Container(
-      child: LeftCategoryNav(),
+//      child: LeftCategoryNav(),
 //      child: Center(
 //        child: Text('分类页面'),
 //      ),
+    child: Row(
+      children: <Widget>[
+        LeftCategoryNav(),
+        Column(
+          children: <Widget>[
+            RightCategoryNav()
+          ],
+        ),
+      ],
+    ),
     );
   }
 
@@ -90,3 +100,51 @@ class _LeftCategoryNavState extends State<LeftCategoryNav> {
     );
   }
 }
+
+// 右侧小类类别
+class RightCategoryNav extends StatefulWidget {
+  @override
+  _RightCategoryNavState createState() => _RightCategoryNavState();
+}
+
+class _RightCategoryNavState extends State<RightCategoryNav> {
+
+  List list = ['名酒', '宝丰', '北京二锅头'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: ScreenUtil().setHeight(80),
+        width: ScreenUtil().setWidth(570),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12)
+          )
+        ),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: list.length,
+          itemBuilder: (context,index){
+            return _rightInkWell(list[index]);
+          },),
+      ),
+    );
+  }
+
+  Widget _rightInkWell(String item) {
+
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+        child: Text(
+          item,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
+  }
+}
+
