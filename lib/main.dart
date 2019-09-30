b7120fd96b1422429eb4d5d8c5130b42335e12bd
CoderSamz <developersamz@outlook.com>
2019-09-30 19:53:31 +0800

    完成-->第51节：购物车_添加商品

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './provide/category_goods_list.dart';
 import 'package:fluro/fluro.dart';
 import './routers/routes.dart';
 import './provide/details_info.dart';
+import './provide/cart.dart';
 
 
 //void main() => runApp(MyApp());
@@ -18,12 +19,14 @@ void main() {
   var childCategory = ChildCategory();
   var categoryGoodsListProvide = CategoryGoodsListProvide();
   var detailsInfoProvide = DetailsInfoProvide();
+  var cartProvide = CartProvide();
 
   providers
     ..provide(Provider<Counter>.value(counter))
     ..provide(Provider<ChildCategory>.value(childCategory))
     ..provide(Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))
-    ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide));
+    ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))
+    ..provide(Provider<CartProvide>.value(cartProvide));
 
   runApp(ProviderNode(child: MyApp(), providers: providers));
 }
