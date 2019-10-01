30bbf8c8dc6657f9805966dd2797bfff3ad0afe1
CoderSamz <developersamz@outlook.com>
2019-10-01 21:03:03 +0800

    完成-->第52节：购物车_建立数据模型

--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cartInfo.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = "[]";
+  List<CartInfoModel> cartList = [];
 
   // 添加商品到购物车
   save(goodsId, goodsName, count, price, images) async {
@@ -26,6 +30,9 @@ class CartProvide with ChangeNotifier {
       // 如果存在，数量进行+1操作
       if(item['goodsId'] == goodsId) {
         tempList[ival]['count'] = item['count'] + 1;
+        //关键代码-----------------start
+        cartList[ival].count++;
+        //关键代码-----------------end
         isHave = true;
       }
       ival += 1;
@@ -33,21 +40,45 @@ class CartProvide with ChangeNotifier {
 
     // 如果没有，进行增加
     if(!isHave) {
-      tempList.add({
+      //关键代码-----------------start
+      Map<String, dynamic> newGoods = {
         'goodsId':goodsId,
         'goodsName':goodsName,
         'count':count,
         'price':price,
         'images':images,
-      });
+      };
+      tempList.add(newGoods);
+      cartList.add(new CartInfoModel.fromJson(newGoods));
+      //关键代码-----------------end
     }
 
     // 把字符串进行encode操作
     cartString = json.encode(tempList).toString();
     print(cartString);
+    print(cartString.toString());
     // 进行持久化
     prefs.setString('cartInfo', cartString);
+    notifyListeners();
+  }
 
+  // 得到购物车中的商品
+  getCartInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // 获得购物车中的商品，这时候是一个字符串
+    cartString = prefs.getString('cartInfo');
+    // 把cartList进行初始化，防止数据混乱
+    cartList = [];
+    // 判断得到的字符串是否有值，如果不判断会报错
+    if(cartString == null) {
+      cartList = [];
+    }else{
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(new CartInfoModel.fromJson(item));
+      });
+    }
+    notifyListeners();
   }
 
   // 清空购物车
@@ -60,3 +91,43 @@ class CartProvide with ChangeNotifier {
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
